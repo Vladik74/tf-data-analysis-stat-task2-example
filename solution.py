aa45287ccc3d62621f9263cb6190c593655aded1
Vladik74 <72654185+Vladik74@users.noreply.github.com>
2023-04-07 00:34:27 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,8 @@ chat_id = 496613075 # Ваш chat ID, не меняйте название пе�
 def solution(p: float, x: np.array):
     alpha = 1 - p
     k = 2/(56*56)
-    q1 = gamma(len(x), 1).ppf((1-alpha)/2)
-    q2 = gamma(len(x), 1).ppf((1+alpha)/2)
+    x_len = len(x)
+    q1 = gamma(x_len, 1).ppf((1-alpha)/2)
+    q2 = gamma(x_len, 1).ppf((1+alpha)/2)
     sum = np.sum(x)
     return (k*((q1+sum)/2)-1/2, k*((q2+sum)/2)-1/2)
