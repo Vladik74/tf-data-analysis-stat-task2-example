4b8607c71ff650b23256f4dfa365fa7d808c126e
Vladik74 <72654185+Vladik74@users.noreply.github.com>
2023-04-08 00:45:43 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,7 +6,7 @@ from scipy.stats import norm
 
 chat_id = 674922610 # Ваш chat ID, не меняйте название переменной
 
-def solution(p: float, x: np.array) -> float:
+def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
