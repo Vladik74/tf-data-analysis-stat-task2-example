86be0fb9195df2f72bd5b3c56eb7604761501e3b
Vladik74 <72654185+Vladik74@users.noreply.github.com>
2023-04-08 00:41:27 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from scipy.stats import norm
 
+
 chat_id = 674922610 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> float:
@@ -11,5 +12,4 @@ def solution(p: float, x: np.array) -> float:
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
     max = x.max()
-    return (max-((max - 0.008) * norm.ppf(1 - alpha / 2) / np.sqrt(2*len(x))), 
-    max-((max - 0.008) * norm.ppf(1 - alpha / 2) / np.sqrt(2*len(x)))) 
+    return (max - ((max - 0.008) * norm.ppf(1 - alpha / 2) / np.sqrt(2*len(x))), max + ((max - 0.008) * norm.ppf(1 - alpha / 2) / np.sqrt(2*len(x)))) 
